4d958352c5f83e4f1a7ec6e4721b60bf40daa3b4
azgardTech <khreibi.mohamed@gmail.com>
2016-09-25 02:26:25 +0200

    school project

---
+++ src/monmagasin/MonMagasin.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monmagasin;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Azgard
+ */
+public class MonMagasin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Scanner s = new Scanner(System.in);
+        char cmd = ' ';
+        Magasin m=new Magasin(0,"Menzah 6",50);
+        do {
+
+            do {
+                System.out.println("0:to exit");
+                System.out.println("1:to add product");
+                System.out.println("2:to list products");
+                System.out.println("3:to search product");
+                cmd = s.next().charAt(0);
+            } while ((cmd != '0') && (cmd != '1') && (cmd != '2') && (cmd != '3'));
+
+            switch (cmd) {
+                case '1':m.AjouterProduit(Produit.SaisieProduit());break;
+                case '2':m.AfficherProduit();break;
+                case '3':m.ChercherProduit(Produit.SaisieProduit());break;
+            }
+        } while (cmd != '0');
+       
+
+       
+
+    }
+}
